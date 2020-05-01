e64ce215cbb2e4d717ab32c1e8a3be0997be353d
Brook Beyene <brookbeyene37@gmail.com>
2020-05-01 04:44:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,16 +1,20 @@
 def using_include(array, element)
+  array.include(element)
 
 end
 
 def using_sort(array)
+  array = array.sort
 
 end
 
 def using_reverse(array)
+  array.reverse
 
 end
 
 def using_first(array)
+  
 
 end
 
